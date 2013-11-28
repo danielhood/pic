cea75336d61b97b3fd91b66cb55d61b1a8632343
Daniel Hood <danielhood@hotmail.com>
2013-11-28 01:18:03 -0500

    Added wavgen project

--- PIC16F819/c/wavgen/wavgen.v1.0.X/main.c
+++ PIC16F819/c/wavgen/wavgen.v1.0.X/main.c
@@ -10,7 +10,19 @@
 //config bits that are part-specific for the PIC16F819
 __CONFIG(FOSC_INTOSCIO & WDTE_OFF & PWRTE_ON & MCLRE_OFF & BOREN_ON & LVP_OFF & CPD_OFF & CP_OFF & WRT_OFF & DEBUG_OFF & CCPMX_RB3);
 
+static unsigned int duty;
+
+static void update_duty(void)
+{
+    CCP1Y = duty;
+    duty >>= 1;
+    CCP1X = duty;
+    duty >>= 1;
+    CCPR1L = duty;
+}
+
 void main(void) {
+    duty = 110;
     // Set internal osc freq to 8MHz
     IRCF0 = 1;
     IRCF1 = 1;
@@ -21,10 +33,17 @@ void main(void) {
     TRISA = 0;      // All pins as output
     PORTA = 0b01010001; // Initial value
 
-    // PortB Config (all digital outs)
-    nRBPU = 1;      // Disable weak pullups
-    TRISB = 0;      // All digital outs
-    PORTB = 0b01010001; // Initial value
+    // PWM Mode on RB3
+    PR2 = 0xFF;     // Maximum PWM period
+    update_duty();
+    //CCPR1L = 0x00;  // Initial duty of 0
+    //CCP1Y = 0;
+    //CCP1X = 0;
+    TRISB = 0;      // Port B outputs
+    T2CKPS1 = 0;    // Prescalre of 1
+    T2CKPS0 = 0;
+    CCP1M3 = 1;     // Enable PWM Mode
+    CCP1M2 = 1;
 
     // Setup timer0
     T0CS = 0;   // Timer mode
@@ -43,6 +62,9 @@ static void interrupt isr(void) {
     {
         TMR0IF = 0; // Clear interrupt
         //PORTA++;
-        //PORTB++;
+        duty++;
+        if (duty >= 1024) duty = 0;
+        update_duty();
     }
 }
+
